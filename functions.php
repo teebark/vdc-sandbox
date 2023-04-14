5c33470c41a99ad9ba5100c9ee9e45f74249016c
teebark <terry@teebark.com>
2023-04-14 11:08:45 -0400

    store locator contact info

--- functions.php
+++ functions.php
@@ -83,6 +83,43 @@ function custom_meta_box_fields( $meta_fields ) {
 		'course_notes_5' => array(
             'label' => __( 'Notes', 'wpsl' )
 		)
+	);
+	/* Add featured store checkbox */
+    $meta_fields[__( 'Location', 'wpsl' )] = array(
+        'featured_dealer' => array(
+            'label' => __( 'Featured', 'wpsl' ),
+            'type'  => 'checkbox'
+        ),
+        'address' => array(
+            'label'    => __( 'Address', 'wpsl' ),
+            'required' => true
+        ),
+        'address2' => array(
+            'label' => __( 'Address 2', 'wpsl' )
+        ),
+        'city' => array(
+            'label'    => __( 'City', 'wpsl' ),
+            'required' => true
+        ),
+        'state' => array(
+            'label' => __( 'State', 'wpsl' )
+        ),
+        'zip' => array(
+            'label' => __( 'Zip Code', 'wpsl' )
+        ),
+        'country' => array(
+            'label'    => __( 'Country', 'wpsl' ),
+            'required' => true
+        ),
+        'country_iso' => array(
+            'type' => 'hidden'
+        ),
+        'lat' => array(
+            'label' => __( 'Latitude', 'wpsl' )
+        ),
+        'lng' => array(
+            'label' => __( 'Longitude', 'wpsl' )
+        )
     );
 
     return $meta_fields;
@@ -91,7 +128,10 @@ function custom_meta_box_fields( $meta_fields ) {
 add_filter( 'wpsl_frontend_meta_fields', 'custom_frontend_meta_fields' );
 
 function custom_frontend_meta_fields( $store_fields ) {
-
+	/* Featued store */
+	$store_fields['wpsl_featured_dealer'] = array(
+        'name' => 'featured_dealer'
+		);
     $store_fields['wpsl_course_title_1'] = array( 
         'name' => 'course_title_1',
         'type' => 'text'
@@ -196,8 +236,25 @@ function custom_listing_template() {
     $listing_template .= "\t\t\t\t" . '<span>' . wpsl_address_format_placeholders() . '</span>' . "\r\n";
     $listing_template .= "\t\t\t\t" . '<span class="wpsl-country"><%= country %></span>' . "\r\n";
     $listing_template .= "\t\t\t" . '</p>' . "\r\n";
-    
-    // Check if the variable contains data before including it.
+	
+	// Show the phone, fax or email data if they exist.
+	/*
+    if ( $wpsl_settings['show_contact_details'] ) {
+        $listing_template .= "\t\t\t" . '<p class="wpsl-contact-details">' . "\r\n";
+        $listing_template .= "\t\t\t" . '<% if ( phone ) { %>' . "\r\n";
+        $listing_template .= "\t\t\t" . '<span><strong>' . esc_html( $wpsl->i18n->get_translation( 'phone_label', __( 'Phone', 'wpsl' ) ) ) . '</strong>: <%= formatPhoneNumber( phone ) %></span>' . "\r\n";
+        $listing_template .= "\t\t\t" . '<% } %>' . "\r\n";
+        $listing_template .= "\t\t\t" . '<% if ( fax ) { %>' . "\r\n";
+        $listing_template .= "\t\t\t" . '<span><strong>' . esc_html( $wpsl->i18n->get_translation( 'fax_label', __( 'Fax', 'wpsl' ) ) ) . '</strong>: <%= fax %></span>' . "\r\n";
+        $listing_template .= "\t\t\t" . '<% } %>' . "\r\n";
+        $listing_template .= "\t\t\t" . '<% if ( email ) { %>' . "\r\n";
+        $listing_template .= "\t\t\t" . '<span><strong>' . esc_html( $wpsl->i18n->get_translation( 'email_label', __( 'Email', 'wpsl' ) ) ) . '</strong>: <%= email %></span>' . "\r\n";
+        $listing_template .= "\t\t\t" . '<% } %>' . "\r\n";
+        $listing_template .= "\t\t\t" . '</p>' . "\r\n";
+    }
+	*/
+	
+    // Course data
 	$listing_template .= "\t\t\t" . '<% if ( course_title_1) { %>' . "\r\n";
     $listing_template .= "\t\t\t" . '<div>Course Title: <%= course_title_1 %></div>' . "\r\n";
     $listing_template .= "\t\t\t" . '<% } %>' . "\r\n"; 
@@ -273,16 +330,48 @@ function custom_listing_template() {
 	
 	$listing_template .= "\t\t\t" . '<p>' . "\r\n";
 
+    $listing_template .= "\t\t\t" . wpsl_more_info_template() . "\r\n"; // Check if we need to show the 'More Info' link and info
     $listing_template .= "\t\t" . '</div>' . "\r\n";
-	
-    // Check if we need to show the distance.
+    $listing_template .= "\t\t" . '<div class="wpsl-direction-wrap">' . "\r\n";
+
     if ( !$wpsl_settings['hide_distance'] ) {
-        $listing_template .= "\t\t" . '<%= distance %> ' . esc_html( $wpsl_settings['distance_unit'] ) . '' . "\r\n";
+        $listing_template .= "\t\t\t" . '<%= distance %> ' . esc_html( $wpsl_settings['distance_unit'] ) . '' . "\r\n";
     }
- 
-    $listing_template .= "\t\t" . '<%= createDirectionUrl() %>' . "\r\n"; 
-    $listing_template .= "\t" . '</li>' . "\r\n"; 
+
+    $listing_template .= "\t\t\t" . '<%= createDirectionUrl() %>' . "\r\n"; 
+    $listing_template .= "\t\t" . '</div>' . "\r\n";
+    $listing_template .= "\t" . '</li>';
 
     return $listing_template;
+}
+/* Add favorite store to results */
+add_filter( 'wpsl_store_data', 'custom_store_data_sort' );
+
+function custom_store_data_sort( $stores ) {
+
+    // Create the array that holds the featured locations
+    $featured_list = array();
+
+    // Loop over the collected location list
+    foreach ( $stores as $k => $store ) {
+
+        // Check if the location is a featured one
+        if ( isset( $store['featured_dealer'] ) && $store['featured_dealer'] ) {
+
+            // Move the featured locations to a new array
+            $featured_list[] = $store;
+
+            // Remove the featured location from the existing $stores array
+            unset( $stores[$k] );
+        }
+    }
+
+    /**
+     * Merge the list of premium locations with the existing list.
+     * This will make sure the premium location show up before the normal locations.
+     */
+    $results = array_merge( $featured_list, $stores );
+
+    return $results;
 }
 ?>
